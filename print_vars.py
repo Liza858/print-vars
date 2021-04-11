36f5b5760d3ed62a6d661a8fdeaf4dff2ca300da
Liza <liza8361@yandex.ru>
2021-04-11 22:49:12 +0300

    add example

--- print_vars.py
+++ print_vars.py
@@ -1,5 +1,20 @@
 import sys
 
+import numpy as np
+
+
+class MyClass:
+    def __init__(self):
+        pass
+
+
+def foo():
+    a = 1
+    b = MyClass()
+    c = [1, 2, 3]
+    d = np.array([1, 2, 3])
+    print_vars()
+
 
 def print_vars():
     frame = sys._getframe(1)
@@ -7,3 +22,7 @@ def print_vars():
         value = frame.f_locals[name]
         is_builtin = value.__class__.__module__ == 'builtins'
         print('{0}: {1}'.format(name, is_builtin))
+
+
+if __name__ == "__main__":
+    foo()
